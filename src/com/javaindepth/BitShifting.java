2695a8a0e4499ede0f315a1ddf24303243c3066e
lightencc <chen_x22@yeah.net>
2017-03-13 21:59:49 +0800

    bit operation

---
+++ src/com/javaindepth/BitShifting.java
@@ -0,0 +1,22 @@
+package com.javaindepth;
+
+public class BitShifting {
+
+    public static void main(String[] args) {
+        /*
+            Decimal     Binary
+            8           00001000
+            2           00000010
+         */
+        System.out.println(8 >> 2);
+
+        /*
+            Decimal     Binary
+            8           00001000
+            32          00100000
+         */
+        System.out.println(8 << 2);
+
+
+    }
+}
--- src/com/javaindepth/Main.java
+++
@@ -1,8 +0,0 @@
-package com.javaindepth;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
